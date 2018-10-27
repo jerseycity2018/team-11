5d07c20f66959a1d5fe82119ceece952302f69c1
Karishma Muni <Karishma.Muni54@qmail.cuny.edu>
2018-10-26 23:47:26 -0400

    adding new files

--- monkeySeeMonkeyDo/monkeySeeMonkeyDo/ViewController.swift
+++ monkeySeeMonkeyDo/monkeySeeMonkeyDo/ViewController.swift
@@ -7,9 +7,25 @@
 //
 
 import UIKit
-
+import AVKit
 class ViewController: UIViewController {
 
+    @IBAction func onClick(_ sender: Any) {
+        
+        if let path = Bundle.main.path(forResource: "video", ofType: "mp4")
+        {
+            
+        let video = AVPlayer(url: URL(fileURLWithPath: path))
+        let videoPlayer = AVPlayerViewController()
+            videoPlayer.player = video
+            
+        present(videoPlayer, animated: true, completion: {
+            
+            video.play()
+            
+            })
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
